05a7fa2bf7756b5eea75fa609045e9467107383b
John3771 <sokolnumber1@gmail.com>
2026-01-08 11:38:08 +0300

    initial release 2

---
+++ CodeBreaker/CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Gleb on 8.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
